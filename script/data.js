8c9911fcae9f35903e57a8a823efb23e818eb752
tahirbhalli <tahirbhalli16@gmail.com>
2020-06-23 19:13:49 +0500

    complete

--- script/data.js
+++ script/data.js
@@ -1,8 +1,9 @@
 class Book {
-  constructor(title, author, price) {
+  constructor(title, author, price, status) {
     this.title = title;
     this.author = author;
     this.price = price;
+    this.status = status;
   }
 }
 
@@ -12,18 +13,41 @@ function create(param) {
   return td;
 }
 let books;
+function btnclicked(btn, tbook) {
+  if (btn.innerText === 'Read') {
+    btn.innerText = 'unread';
+  } else {
+    btn.innerText = 'Read';
+  }
+  books.forEach(book => {
+    if (book === tbook) {
+      book.status = btn.innerText;
+    }
+  });
+  localStorage.setItem('books', JSON.stringify(books));
+}
+function createbutton(book) {
+  const btn = document.createElement('button');
+  btn.innerText = book.status;
+  btn.className = 'btn btn-primary m-2';
+  btn.onclick = () => btnclicked(btn, book);
+  return btn;
+}
 function addbook(book) {
   const td = document.getElementById('books');
   const row = document.createElement('tr');
   row.appendChild(create(book.title));
   row.appendChild(create(book.author));
   row.appendChild(create(book.price));
+
+
+  row.appendChild(createbutton(book));
   td.appendChild(row);
 }
 // eslint-disable-next-line no-unused-vars
 function getbooks() {
   if (localStorage.getItem('books') === null) {
-    books = [new Book('javascript', 'Ghosh', 10), new Book('css', 'Ghosh', 10), new Book('html', 'Ghosh', 10)];
+    books = [new Book('javascript', 'Ghosh', 10, 'Read'), new Book('css', 'Ghosh', 10, 'Read'), new Book('html', 'Ghosh', 10, 'Read')];
   } else {
     books = JSON.parse(localStorage.getItem('books'));
   }
@@ -35,8 +59,7 @@ function getbooks() {
 
 // eslint-disable-next-line no-unused-vars
 function add(title, author, price) {
-  books.push(new Book(title, author, price));
+  books.push(new Book(title, author, price, 'Read'));
   localStorage.setItem('books', JSON.stringify(books));
-  addbook(new Book(title, author, price));
-  document.getElementById('closebtn').click(true);
+  addbook(new Book(title, author, price, 'Read'));
 }
